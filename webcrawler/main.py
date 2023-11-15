ee347b6fbbcf9b43321582ad5792b9d74d9add88
Felicitas Lock <89555492+Felinchen76@users.noreply.github.com>
2023-11-15 22:06:48 +0100

    enabled search for dynamic websites important: needs selenium version 4.0 or above to work!

--- webcrawler/main.py
+++ webcrawler/main.py
@@ -1,35 +1,51 @@
-#wieso geht das???
-#selenium für das laden dynamischer inhalte
-import bs4
-from bs4 import BeautifulSoup
-import requests
-import cssutils
+import PySimpleGUI as sg
 import queue
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-url = 'https://de.wikipedia.org/wiki/Schwarzes_Loch'
+# GUI erstellen
+window = sg.Window(title="GRAKA Crawler", layout=[
+    [sg.Text('Search for best price graphic card model xyz', justification='center')],
+    [sg.Button('Crawl')]],
+    margins=(300, 150))
+
+url = 'https://duckduckgo.com/?q=grafikkarten+kaufen&ia=web'
+
 queue = queue.Queue()
 limit = 500
-
-def get_url_content(url):
-    return requests.get(url).text
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 def get_blog_content(url):
-    content = get_url_content(url)
-    soup = BeautifulSoup(content, "html.parser")
-
-    #einfach alle Links von der Seite ziehen
-    for post in soup.findAll('a'):
-        #damit nur die eigentlichen Linktexte ausgegeben werden und nicht die tags ebenfalls
-        link = post.get('href')
-        print('Link: %s' % link)
-        if queue.qsize() < limit:
-            queue.put(link)
-
-get_blog_content(url)
-
-while queue.qsize() != 0:
     try:
-        get_blog_content(queue.get())
-        limit = limit - 1
-    except Exception:
-        print('no valid url')
+        #set_playwright_browsers_path()
+        driver.get(url)
+
+        articles = driver.find_elements(By.CLASS_NAME,'wLL07_0Xnd1QZpzpfR4W')
+
+        for article in articles:
+            #vllt bietet sich hier was anderes mehr an...? -> ich muss ja nun auch auf anderen Seiten suchen können
+            link = article.find_element(By.XPATH,'..//div[2]/h2/a').get_attribute('href')
+            print('Link: %s' % link)
+            if queue.qsize() < limit:
+                if 'youtube' not in link:
+                    queue.put(link)
+
+    except Exception as e:
+        print(f'Fehler beim Verarbeiten der URL {url}: {e}')
+    finally:
+        # Schließe den Browser
+        driver.quit()
+def main():
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Crawl":
+            get_blog_content(url)
+
+    window.close()
+
+main()
